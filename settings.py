41fb7bbf6625370818f5e4057aef73bb397f233c
ahmad mustafa <ahmad.m.abdelqader@gmail.com>
2021-09-07 22:43:51 +0300

    added segmentation preprocessing code

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@
 
 
 dicom_path = 'C:/Users/RadioscientificOne/PycharmProjects/Stroke-Detection/dataset/'
-png_out_path = 'C:/Users/RadioscientificOne/PycharmProjects/Stroke-Detection/output/'#extracted_png_bone/'
+png_out_path = 'F:/Cengiz/Stroke-Detection/output'#extracted_png_bone/'
